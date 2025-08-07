d910ce0e38439af60a72a178c08afdb4d81aef54
gameoffc12 <gameoffc12@gmail.com>
2025-08-07 19:27:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/api/build', async (req, res) => {
